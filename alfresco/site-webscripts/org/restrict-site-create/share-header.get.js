290033cab69d1179fc7d33f5970a94ee9056d2ab
Fintan MacMahon <fintan.macmahon@gmail.com>
2014-12-17 09:08:25 +0000

    Initail commit

---
+++ alfresco/site-webscripts/org/restrict-site-create/share-header.get.js
@@ -0,0 +1,9 @@
+// Find the "Sites" menu...
+var sitesMenu = widgetUtils.findObject(model.jsonModel, "id", "HEADER_SITES_MENU");
+if (sitesMenu != null)
+{
+   // Hide the site finder...
+    if (!user.isAdmin) {
+        sitesMenu.config.showCreateSite = false;
+    }
+}
