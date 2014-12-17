f8572d4e73d31966c44b1fcb2d25efb72a60b37a
Fintan MacMahon <fintan.macmahon@gmail.com>
2014-12-17 09:26:12 +0000

    Added js to restrict create button.

--- alfresco/site-webscripts/org/restrict-site-create/share-header.get.js
+++ alfresco/site-webscripts/org/restrict-site-create/share-header.get.js
@@ -1,8 +1,7 @@
 // Find the "Sites" menu...
 var sitesMenu = widgetUtils.findObject(model.jsonModel, "id", "HEADER_SITES_MENU");
-if (sitesMenu != null)
-{
-   // Hide the site finder...
+if (sitesMenu != null) {
+    // Hide the site finder...
     if (!user.isAdmin) {
         sitesMenu.config.showCreateSite = false;
     }
